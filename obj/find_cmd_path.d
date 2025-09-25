a2cebd939023d8dd33f73937f09590d838608fe8
川口拓郎 <kawaguchitakuro@kawaguchitakurounoMacBook-Air.local>
2025-09-25 09:53:14 +0900

    machennsyu1

--- obj/find_cmd_path.d
+++ obj/find_cmd_path.d
@@ -1,11 +1,7 @@
 obj/find_cmd_path.o: src/find_cmd_path.c minishell.h \
   libft/includes/libft.h libft/includes/ft_printf.h \
   libft/includes/get_next_line.h
-
 minishell.h:
-
 libft/includes/libft.h:
-
 libft/includes/ft_printf.h:
-
 libft/includes/get_next_line.h:
